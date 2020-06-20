58451f9e0158c783ef338ea8a1d970800f35ca72
Paras Chodavadiya <paras.chodavadiya@gmail.com>
2020-06-20 10:37:48 +0530

    - add Gitignore file to prevent unnecessary files

--- api/service/index.js
+++
@@ -1,8 +0,0 @@
-export {
-  restApiGet,
-  restApiPost,
-  restApiPut,
-  restApiPatch,
-  restApiDelete,
-  restApiMultiPartPost
-} from "./ApiMethod";
--- apiUrls/index.js
+++
@@ -1,2 +0,0 @@
-//API URL List
-export const getCatalogs_url = "/catalogs?verbose=true"; // TANGO Gift Card API
